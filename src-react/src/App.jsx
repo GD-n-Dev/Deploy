276b732c2aff6088b4588d4a4ef602c43b360471
Kevin Albarran <recodedkevdev@gmail.com>
2024-01-20 22:01:04 -0800

    changed some configs in order to be able to talk to the local maching

--- src-react/src/App.jsx
+++ src-react/src/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { open } from '@tauri-apps/api/dialog';
 
 const App = () => {
-  return <h1>From sdfsdfsdfsd</h1>;
+  const clickHandler = async () => {
+    const selectedFiles = await open({
+      title: 'Select your file',
+    });
+
+    console.log(selectedFiles);
+  };
+
+  return (
+    <>
+      <button onClick={clickHandler}>select files</button>
+    </>
+  );
 };
 
 export default App;
