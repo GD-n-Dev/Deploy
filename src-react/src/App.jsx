1b701a408d031846ae0577c2e4aaa799ba23e195
Kevin Albarran <recodedkevdev@gmail.com>
2024-01-21 23:25:04 -0800

    set up some basis for working with bitbuckets api

--- src-react/src/App.jsx
+++ src-react/src/App.jsx
@@ -1,18 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import { invoke } from '@tauri-apps/api';
 
-const checkIsGitInstalled = async () => await invoke('check_git_installed');
+const BITBUCKET_OAUTH_URL = 'https://bitbucket.org/site/oauth2/authorize';
+const CLIENT_ID = 'key';
+const REDIRECT_URI = 'http://localhost:3000';
 
 const App = () => {
-  const [isGitInstalled, setIsGitInstalled] = useState(false);
+  if (window.location.href.startsWith(REDIRECT_URI)) {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get('code');
+    console.log('getting code', code);
+  }
 
-  useEffect(() => {
-    const installed = checkIsGitInstalled();
+  const handleClick = () => {
+    window.location.href = `${BITBUCKET_OAUTH_URL}?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`;
+  };
 
-    installed.then((res) => setIsGitInstalled(res)).catch((err) => console.log(err));
-  }, []);
-
-  return <>{isGitInstalled ? <h1>App</h1> : <h2>No App</h2>}</>;
+  return (
+    <>
+      <button onClick={handleClick}>Click me</button>
+    </>
+  );
 };
 
 export default App;
