b486daa2ac53a064fec4c864b9b4082f0ab8f7a5
Kevin Albarran <recodedkevdev@gmail.com>
2024-01-23 01:16:13 -0800

    added new logic in order to pull configurations from a config file instead of being hardcoded

--- src-react/src/App.jsx
+++ src-react/src/App.jsx
@@ -1,87 +1,54 @@
 import React, { useEffect, useState } from 'react';
+import { invoke } from '@tauri-apps/api/tauri';
 
 const BITBUCKET_OAUTH_URL = 'https://bitbucket.org/site/oauth2/authorize';
-const CLIENT_ID = 'myId';
-const CLIENT_SECRET = 'mySecret';
-const REDIRECT_URI = 'http://localhost:3000';
+const CLIENT_ID = 'id';
+const REDIRECT_URI = 'redirecturi';
 
-const exchangeCodeForToken = async (code) => {
-  const url = 'https://bitbucket.org/site/oauth2/access_token';
-  const params = new URLSearchParams();
-
-  params.append('grant_type', 'authorization_code');
-  params.append('code', code);
-  params.append('redirect_uri', REDIRECT_URI);
-  params.append('client_id', CLIENT_ID);
-  params.append('client_secret', CLIENT_SECRET); // Securely manage this
-
-  const requestOptions = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: params,
-  };
-
+const sendCodeToBackend = async (code) => {
   try {
-    const response = await fetch(url, requestOptions);
-    const data = await response.json();
-
-    if (response.ok) {
-      return data.access_token;
-    } else {
-      console.error('Error exchanging code for token:', data);
-      return null;
-    }
+    const data = await invoke('exchange_code_for_token', { authCode: { code } });
+    console.log(data);
   } catch (error) {
     console.error('Error exchanging code for token', error);
-    return null;
-  }
-};
-
-const makeApiRequest = async (accessToken) => {
-  const url = 'https://api.bitbucket.org/2.0/repositories/{workspace}';
-  const requestOptions = {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-      Accept: 'application/json',
-    },
-  };
-
-  try {
-    const response = await fetch(url, requestOptions);
-    const data = await response.json();
-
-    if (response.ok) {
-      // Process the response data
-      console.log(data);
-    } else {
-      console.error('Error making API request:', data);
-    }
-  } catch (error) {
-    console.error('Error making API request', error);
   }
 };
 
 const App = () => {
-  if (window.location.href.startsWith(REDIRECT_URI)) {
+  const [config, setConfig] = useState({});
+
+  useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const code = urlParams.get('code');
-    const token = exchangeCodeForToken(code);
 
-    token
-      .then((res) => {
-        makeApiRequest(res);
-      })
-      .catch((err) => console.log(err));
-  }
+    if (code) {
+      sendCodeToBackend(code).catch((err) => console.error(err));
+    }
+  }, [window.location.href]);
+
+  useEffect(() => {
+    getConfig();
+  }, []);
 
   const handleClick = () => {
     window.location.href = `${BITBUCKET_OAUTH_URL}?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`;
   };
 
+  const getConfig = async () => {
+    const config = await invoke('get_config');
+    setConfig(JSON.parse(config));
+  };
+
+  const handleClickGetConfig = () => {
+    const config = getConfig();
+  };
+
+  console.log(config);
+
   return (
     <>
       <button onClick={handleClick}>Click me</button>
+      <button onClick={handleClickGetConfig}>GetConfig</button>
     </>
   );
 };
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,55 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
+// For check_git_installed
 use std::process::Command;
 use std::process::Stdio;
+
+// For exchange_code_for_token
+use tauri::{command, App, Manager, Window, State};
+use reqwest::Client;
+use serde::Deserialize;
+
+// For reading config file
+use serde::Serialize;
+use std::fs;
+use std::io;
+use std::path::PathBuf;
+use std::io::{Read};
+use std::env;
+use std::path::Path;
+
+struct AuthClient {
+    client: Client,
+}
+
+#[derive(Deserialize)]
+struct AuthCode {
+    code: String,
+}
+
+
+#[tauri::command]
+async fn exchange_code_for_token(auth_client: State<'_, AuthClient>, auth_code: AuthCode) -> Result<String, String> {
+    let response = auth_client.client.post("https://bitbucket.org/site/oauth2/access_token")
+        .form(&[
+            ("grant_type", "authorization_code"),
+            ("code", &auth_code.code),
+            ("redirect_uri", "uri"),
+            ("client_id", "key"), // Replace with actual client ID
+            ("client_secret", "secret"), // Replace with actual client secret
+        ])
+        .send()
+        .await;
+
+    match response {
+        Ok(res) => {
+            let token_data = res.text().await.unwrap(); // Handle this properly
+            Ok(token_data)
+        }
+        Err(err) => Err(err.to_string()),
+    }
+}
+
 
 #[tauri::command] // Mark this function as a Tauri command
 fn check_git_installed() -> bool {
@@ -16,10 +63,67 @@ fn check_git_installed() -> bool {
     }
 }
 
+// #[derive(Debug, Serialize)]
+// struct CustomError {
+//     message: String,
+// }
+
+// impl From<std::io::Error> for CustomError {
+//     fn from(error: std::io::Error) -> Self {
+//         CustomError {
+//             message: error.to_string(),
+//         }
+//     }
+// }
+
+// #[command]
+// async fn get_config() -> Result<String, CustomError> {
+//     let mut path = env::current_dir()?;
+//     path.push("../config/test.json");
+
+//     let mut file = fs::File::open(path)?;
+//     let mut contents = String::new();
+//     file.read_to_string(&mut contents)?;
+    
+//     Ok(contents)
+// }
+
+#[derive(Debug, serde::Serialize)]
+struct CustomError {
+    message: String,
+}
+
+impl From<std::io::Error> for CustomError {
+    fn from(error: std::io::Error) -> Self {
+        CustomError {
+            message: error.to_string(),
+        }
+    }
+}
+
+#[command]
+async fn get_config() -> Result<String, CustomError> {
+    let current_directory = env::current_dir().map_err(|_| CustomError { message: "Failed to get current directory".to_owned() })?;
+
+    let new_path = current_directory.join("config").join("test.json");
+
+    let mut file = fs::File::open(new_path)?;
+
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
 
 fn main() {
     tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![check_git_installed])
+    .manage(AuthClient {
+        client: Client::new(),
+    })
+    .invoke_handler(tauri::generate_handler![exchange_code_for_token,
+         get_config
+         ])
     .run(tauri::generate_context!())
-    .expect("eror while running tauri appliction");
+    .expect("error while running tauri application");
 }
