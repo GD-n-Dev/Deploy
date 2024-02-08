9aeab1895f42ce01742068982f31e7257445f5fc
Jonathan Lister <jlister@inland-engineering.com>
2024-02-07 17:17:23 -0800

    adding custom css

--- src-react/src/App.jsx
+++ src-react/src/App.jsx
@@ -2,46 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { invoke } from '@tauri-apps/api/tauri';
 
 
-
 const App = () => {
-  const [config, setConfig] = useState({});
-  const [gitStatus, setGitStatus] = useState("");
-
-  useEffect(() => {checkGit()}, [])
-
-  const gitPull = async () => {
-    invoke('git_pull')
-      .then((res) => {
-        const result = res;
-        console.log('result: ', result);
-      }).catch((err) => console.error(err));
-  };
-  const getConfig = async () => {
-    invoke('get_config')
-      .then((res) => {
-        const result = JSON.parse(res);
-        console.log("result: ", result);
-        setConfig(result);
-      }).catch((err) => console.error(err))
-  };
-
-  const checkGit = async () => {
-    invoke('check_git_installed')
-      .then((res) => {
-        const result = res;
-        console.log("git result: ", result);
-        setGitStatus(result);
-      }).catch((err) => console.error(err))
-  };
-
-
-  console.log('config: ', config);
 
   return (
     <>
-      <button onClick={() => gitPull()} style={{ margin: '25px' }}>git pull</button>
-      <button onClick={() => getConfig()} style={{ margin: '25px' }}>get config</button>
-      <button onClick={() => checkGit()} style={{ margin: '25px' }}>Check Git Version</button>
+      <button className='button' onClick={() => {}}>Deploy</button>
+      <button className='button' onClick={() => {}}>Revert to stable version</button>
       <p>{gitStatus}</p>
     </>
   );
