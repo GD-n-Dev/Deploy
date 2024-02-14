30720d688f321b36c8c1a910890cd8866b266245
Jonathan Lister <jlister@inland-engineering.com>
2024-02-13 17:42:56 -0800

    Trying to setup reading the config file

--- src-react/src/App.jsx
+++ src-react/src/App.jsx
@@ -5,27 +5,26 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 const App = () => {
-  const [path, setPath] = useState("");
+  const [path, setPath] = useState('');
+  const [config, setConfig] = useState('');
 
   const getConfig = async () => {
-    invoke('get_config').then((res) => console.log(res)).catch((err) => console.error(err));
+    invoke('get_config').then((res) => setConfig(JSON.parse(res))).catch((err) => console.error(err));
   }
 
   return (
     <>
-    <div style={{ height: '100%', display: 'flex', placeContent: 'center', alignItems: 'center' }}>
-      <div>
-        <input className='form-input' value={path} onChange={(e) => {
-          const {target: {id, name, value}} = e;
-          setPath(value);
-        }} placeholder='Deployment Location' />
-        <button className='button' onClick={() => {}}>Browse</button>
-      </div>
+    <div style={{ height: '100%', display: 'flex', placeContent: 'center', flexDirection: 'column', alignItems: 'center' }}> 
       <div>
         <button className='button' onClick={() => {
           toast.info('Deployment started');
         }}>Deploy</button>
-        <button className='button' onClick={() => {}}>Revert to stable version</button>
+        <button className='button' onClick={() => {
+          toast.success('Reverted to Stable version completed');
+        }}>Revert to stable version</button>
+      </div>
+      <div>
+        <p>{config} - config file</p>
       </div>
     </div>
     <ToastContainer theme="dark"/>
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,15 +6,14 @@
 
 use std::process::{Command, Stdio};
 
+use std::{fs, env};
+use std::io::{self, Read, Write};
+use std::path::{Path, PathBuf};
+
 use serde_json::Value;
 use tauri::{command, App, Manager, Window, State};
 use reqwest::Client;
 use serde::{Serialize, Deserialize};
-
-use std::fs;
-use std::io::{self, Read};
-use std::path::{Path, PathBuf};
-use std::env;
 
 
 fn main() {
@@ -35,7 +34,24 @@ fn get_config() -> String {
    raw_json
 }
 
-#[tauri::command] // Tauri Command
-fn deploy_main(deploy_path: String) {
-    
+#[tauri::command]
+fn get_status() -> String {
+    match Command::new("git")
+        .arg("pull")
+        .output()
+        {
+            Ok(output) => String::from_utf8_lossy(&output.stdout).to_string(),
+            Err(err) => err.to_string(),
+        }
+}
+
+#[tauri::command]
+fn deploy_main() {
+    let config: String = get_config();
+    // Get config value to get live SEER path
+    // Get config value backup SEER path
+    // Backup SEER and create new folder with date and version
+
+    // when backup is completed then go ahead and robocopy overwrite build files into live SEER
+    // folder
 }
